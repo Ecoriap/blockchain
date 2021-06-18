5afb0dd77395240afd376ff97be97479e6077ed7
Ecoriap <ecoriap@protonmail.com>
2021-06-18 10:46:36 +0200

    Update hashage.py

--- hashage.py
+++ hashage.py
@@ -1,5 +1,6 @@
 from hashlib import sha512
 import getpass
+import json
 
 
 #creation et hashage du mot de passe de reference
@@ -18,13 +19,21 @@ user_mdp = user_mdp.encode()
 user_mdp_sign = sha512(user_mdp).hexdigest()
 print (user_mdp_sign)
 
-
+adresseEnvoyeur = "0x214989565878995789759964"
+adresseReceveur = "0x555555555555555555555555"
 
 #comparaison du mdp entré par l'utilisateur au mdp de reference
 
 user_message = getpass.getpass('Votre message: ')
 
-user_message = user_message.encode()
+user_message_encode = user_message.encode()
 
-user_message_sign = sha512(user_message).hexdigest()
+user_message_sign = sha512(user_message_encode).hexdigest()
 print (user_message_sign)
+
+
+crypto = getpass.getpass('Nombre de Eco: ')
+
+block = {"Envoyeur": adresseEnvoyeur, "Receveur": adresseReceveur, "Message_chifre": user_message_sign, "Message": user_message, "Ecoria": crypto}
+
+print (block)
